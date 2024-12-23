f70bed0e908e35ce285b785d859b58b060ea73c5
Dreynaldis <dion.reynaldis@gmail.com>
2024-12-23 17:16:52 +0700

    theme changes

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,12 @@
 import Image from "next/image";
 
 export default function Home() {
+  const test = () => {
+    console.log("test");
+  };
   return (
-    <div className="text-3xl font-black" >
-      Welcome to Next flow
+    <div>
+      <h1 className="h1-bold">Welcome to Next flow</h1>
     </div>
   );
 }
--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,6 +1,8 @@
 import { dirname } from "path";
 import { fileURLToPath } from "url";
 import { FlatCompat } from "@eslint/eslintrc";
+import { NewLineKind } from "typescript";
+import { rules } from "eslint-config-prettier";
 
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = dirname(__filename);
@@ -10,7 +12,57 @@ const compat = new FlatCompat({
 });
 
 const eslintConfig = [
-  ...compat.extends("next/core-web-vitals", "next/typescript"),
+  ...compat.extends(
+    "next/core-web-vitals",
+    "next/typescript",
+    "standard",
+    "plugin:tailwindcss/recommended",
+    "prettier"
+  ),
+  {
+    plugins: {
+      prettier: require("eslint-plugin-prettier"),
+    },
+  },
+  {
+    rules: {
+      "import/order": [
+        "error",
+        {
+          groups: [
+            "builtin",
+            "external",
+            "internal",
+            ["parent", "sibling"],
+            "index",
+            "object",
+          ],
+          newlinesBetween: "always",
+          pathGroups: [
+            {
+              pattern: "@app/**",
+              group: "external",
+              position: "after",
+            },
+          ],
+          pathGroupsExcludedImportTypes: ["builtin"],
+          alphabetize: {
+            order: "asc",
+            caseInsensitive: true,
+          },
+        },
+      ],
+    },
+  },
+  {
+    ignores: ["components/ui/**"],
+  },
+  {
+    files: ["*.ts", "*.tsx"],
+    rules: {
+      "no-undef": "off",
+    },
+  },
 ];
 
 export default eslintConfig;
