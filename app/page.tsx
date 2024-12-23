69441aa7d36c2474dc418fa82dfb714f01797965
Dreynaldis <dion.reynaldis@gmail.com>
2024-12-23 18:25:10 +0700

    adding assets

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,6 @@
 import Image from "next/image";
 
 export default function Home() {
-  const test = () => {
-    console.log("test");
-  };
   return (
     <div>
       <h1 className="h1-bold">Welcome to Next flow</h1>
